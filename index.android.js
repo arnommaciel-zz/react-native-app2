311f7c781d5f51eee5f5a0d7cba0824839422ee3
Arnom Maciel <arnom.silva@sgr.com.br>
2017-10-08 13:54:45 -0300

    flex

--- index.android.js
+++ index.android.js
@@ -5,25 +5,61 @@
  */
 
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { Text, AppRegistry, View, TouchableOpacity } from 'react-native';
 
 
 const Styles = {
+  principal: {
+    flex: 1,
+    backgroundColor:'skyblue',
+    height: 300,
+    ///justifyContent: 'space-between',
+    //alignItems: 'center',
+    //flexDirection: 'row'
+  },
   text:{
     fontSize:30,
+    padding:10,
+    alignSelf:'center',
+    color: '#fff',
+    fontWeight: '100'
+  },
+  topo:{
+    flex: 1,
     backgroundColor:'#08509B',
     padding:10,
-    textAlign:'center',
-    shadowColor:'#000',
-    shadowOffset:{
-      width:10, height:15
-    }
+  },
+  conteudo:{
+    flex: 3,
+    backgroundColor:'#2A48FA',
+    padding:10,
+  },
+  rodape:{
+    flex: 2,
+    backgroundColor:'#08509B',
+    padding:10,
+  },
+  botao:{
+    backgroundColor: '#48bbec'
   }
 };
 
 const App = () => {
+  const {principal, topo, conteudo, rodape, text, botao} = Styles;
   return (
-    <Text style={Styles.text}>Frases do dia!</Text>
+    <View style={ principal}>
+      <View style={ topo }>
+        <Text style={text}>Topo</Text>
+      </View>
+      <View style={ conteudo }>
+        <TouchableOpacity style={botao}>
+          <Text style={text}>Clique aqui</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={ rodape }>
+        <Text style={text}>Rodapé</Text>
+      </View>
+    </View>
   );
 };
 
